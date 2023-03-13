02cd015ce7c2270a2645f07edaf0ecaba3c7a538
DavidArias-1509 <davidariass0519@gmail.com>
2023-03-12 23:01:12 -0500

    Update Main_Principal.java

--- EstablecimientoComercial/src/Main_Principal.java
+++ EstablecimientoComercial/src/Main_Principal.java
@@ -1,19 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-
 /**
  *
  * @author david
  */
 public class Main_Principal {
 
-    /**
-     * @param args the command line arguments
-     */
+   
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("nada");
     }
     
+    public static int Menu1() {
+        int option=0;
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        System.out.println("");
+        return option;
+    }
 }
+
