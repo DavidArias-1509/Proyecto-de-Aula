d7e2ed6b6ce9627b761d5196bcc349b0eeaf9ebd
DavidArias-1509 <davidariass0519@gmail.com>
2023-03-26 11:10:21 -0500

    Update Empleado.java

--- EstablecimientoComercial/src/Modelos/Empleado.java
+++ EstablecimientoComercial/src/Modelos/Empleado.java
@@ -9,5 +9,94 @@ package Modelos;
  * @author david
  */
 public class Empleado {
+        private String nombre;
+        private int identificacion;
+        private int telefono;
+        private String email; 
+        private String sexo;
+        private String direccion;
+        private int edad;
+        private double salarioBase;
+
+    public Empleado(){
+        this("Null", 0,0,"Null@","Unsigned","Unsigned",0);
+    }
+    public Empleado(String nombre, int identificacion, int telefono, String email, String sexo, String direccion, int edad) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        this.telefono = telefono;
+        this.email = email;
+        this.sexo = sexo;
+        this.direccion = direccion;
+        this.edad = edad;
+        this.salarioBase=1085670;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public double getSalarioBase() {
+        return salarioBase;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void setSalarioBase(double salarioBase) {
+        this.salarioBase = salarioBase;
+    }
     
-}
+    
+        
+        
+}   
