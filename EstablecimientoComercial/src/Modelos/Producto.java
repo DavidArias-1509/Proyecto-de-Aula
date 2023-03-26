9588d6402a9b6df5bc6af35d3d8f74658f12ea6b
DavidArias-1509 <davidariass0519@gmail.com>
2023-03-26 11:21:06 -0500

    Update Producto.java

--- EstablecimientoComercial/src/Modelos/Producto.java
+++ EstablecimientoComercial/src/Modelos/Producto.java
@@ -9,5 +9,53 @@ package Modelos;
  * @author david
  */
 public class Producto {
+    private String nombre;
+    private int codigo;
+    private int cantidadDisponible;
+    private String descripcion;
+
+   public Producto(){
+       this("Null", 0,0);
+   }
+    public Producto(String nombre, int codigo, int cantidadDisponible) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.cantidadDisponible = cantidadDisponible;
+        this.descripcion="No hay descripcion aun";
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public int getCantidadDisponible() {
+        return cantidadDisponible;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public void setCantidadDisponible(int cantidadDisponible) {
+        this.cantidadDisponible = cantidadDisponible;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
     
 }
