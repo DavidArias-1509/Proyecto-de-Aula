3ff8d5a402e1420574362153c97f220b6a7e9286
ecardenasm <ecardenasm@unicesar.edu.co>
2023-03-26 13:46:48 -0500

    Update Distribuidor.java

--- EstablecimientoComercial/src/Modelos/Distribuidor.java
+++ EstablecimientoComercial/src/Modelos/Distribuidor.java
@@ -4,10 +4,105 @@
  */
 package Modelos;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
 /**
  *
  * @author david
  */
 public class Distribuidor {
+    private String nombre;
+    private String salida;
+    private String destino;
+    private LocalDate fechaSalida;
+    private LocalDate fechaEntrega;
+    private ArrayList<Producto> cargamento;
+    private double valorCargamento;
+    private double deudaDistribuidor;
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the salida
+     */
+    public String getSalida() {
+        return salida;
+    }
+
+    /**
+     * @param salida the salida to set
+     */
+    public void setSalida(String salida) {
+        this.salida = salida;
+    }
+
+    /**
+     * @return the destino
+     */
+    public String getDestino() {
+        return destino;
+    }
+
+    /**
+     * @param destino the destino to set
+     */
+    public void setDestino(String destino) {
+        this.destino = destino;
+    }
+
+    /**
+     * @return the fechaSalida
+     */
+    public LocalDate getFechaSalida() {
+        return fechaSalida;
+    }
+
+    /**
+     * @param fechaSalida the fechaSalida to set
+     */
+    public void setFechaSalida(LocalDate fechaSalida) {
+        this.fechaSalida = fechaSalida;
+    }
+
+    /**
+     * @return the fechaEntrega
+     */
+    public LocalDate getFechaEntrega() {
+        return fechaEntrega;
+    }
+
+    /**
+     * @param fechaEntrega the fechaEntrega to set
+     */
+    public void setFechaEntrega(LocalDate fechaEntrega) {
+        this.fechaEntrega = fechaEntrega;
+    }
+
+    /**
+     * @return the valorCargamento
+     */
+    public double getValorCargamento() {
+        return valorCargamento;
+    }
+
+    /**
+     * @return the deudaDistribuidor
+     */
+    public double getDeudaDistribuidor() {
+        return deudaDistribuidor;
+    }
     
 }
