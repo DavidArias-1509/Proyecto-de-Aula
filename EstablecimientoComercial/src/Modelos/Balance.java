87c5c1fa9bcb16faebc24e5e0ef3880946049d6f
DavidArias-1509 <davidariass0519@gmail.com>
2023-03-26 15:25:12 -0500

    Update Balance.java

--- EstablecimientoComercial/src/Modelos/Balance.java
+++ EstablecimientoComercial/src/Modelos/Balance.java
@@ -13,5 +13,42 @@ import java.util.ArrayList;
 public class Balance {
     private ArrayList<Venta> ventas;
     private ArrayList<Empleado> empleados;
+    private String periodo;
+
+    public Balance(String periodo) {
+        this.periodo = periodo;
+        this.ventas= new ArrayList();
+        this.empleados= new ArrayList();
+    }
+
+    public ArrayList<Venta> getVentas() {
+        return ventas;
+    }
+
+    public ArrayList<Empleado> getEmpleados() {
+        return empleados;
+    }
+
+    public String getPeriodo() {
+        return periodo;
+    }
+
+    public void setVentas(ArrayList<Venta> ventas) {
+        this.ventas = ventas;
+    }
+
+    public void setEmpleados(ArrayList<Empleado> empleados) {
+        this.empleados = empleados;
+    }
+
+    public void setPeriodo(String periodo) {
+        this.periodo = periodo;
+    }
+    
+    
+    
+    
+   
+    
     
 }
