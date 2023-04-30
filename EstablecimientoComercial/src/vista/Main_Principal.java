ed142821287c0f4d0fb88de1f395fec22c0fde39
DavidArias-1509 <davidariass0519@gmail.com>
2023-04-29 20:54:45 -0500

    menu

--- EstablecimientoComercial/src/vista/Main_Principal.java
+++ EstablecimientoComercial/src/vista/Main_Principal.java
@@ -1,5 +1,7 @@
 package vista;
 
+import javax.swing.JOptionPane;
+
 
 /**
  *
@@ -9,7 +11,16 @@ public class Main_Principal {
     
    
     public static void main(String[] args) {
-        
+       char cn='S';
+        while(cn=='S'){
+            int op=Integer.parseInt(JOptionPane.showInputDialog(null,"1- Facturar\n2- platos\n3-Revisar mesa disponible\n4-cerrar"));       
+            switch(op){
+                case 1: JOptionPane.showMessageDialog( null, "Menu");break;
+                case 2: JOptionPane.showMessageDialog( null, "Platos");break;
+                case 3: JOptionPane.showMessageDialog( null, "Mesas");break;
+                case 4: JOptionPane.showMessageDialog( null, "Bye");cn='N';break;  
+            }
+        }
     }
     
     
